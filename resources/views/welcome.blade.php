e75a615f6e97103a966d56f3e52a53169a1cb258
Yash124gamer <y325dde@gmail.com>
2023-08-20 21:28:40 +0530

    updated the Landing page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,22 +2,29 @@
 
 @section('content')
 <div style="background-color: #e8fdf1 ">
-        <div class="row mx-5 d-flex justify-content-between mb-3">
+        <div class="row mx-5 d-flex justify-content-between mb-5">
             <div class="col-md-4" style="margin-top: 8%">
                 <p class="text-wrap fs-1 fw-bold">Handling customer complaints is easy now.</p>
-                <p class="text-wrap fw-medium">We will help you receive, respond to, and resolve customer grievances with simple solutions, and leaves you with more time for tasks that matter.</p>
-                <button type="button" class="btn btn-success">Get Started</button>
+                <p class="text-wrap fs-5 fw-medium">We will help you receive, respond to, and resolve customer grievances with simple solutions, and leaves you with more time for tasks that matter.</p>
+                <button type="button" class="btn btn-success btn-lg">Get Started</button>
             </div>
-            <img class="col-md-5" src="complaint-management-handing.webp" alt="" width="584" height="450">            
+            <img class="col-md-5" src="complaint-management-handing.webp" alt="" width="584" height="490">            
         </div>
 </div>
 <div >
-    <div class="row mx-5 d-flex justify-content-center">
-        <img class="col-md-5" src="desk-customer-connect.webp" alt="" width="484" height="560">            
-        <div class="col-md-4" style="margin-top: 8%">
+    <div class="row mx-5 d-flex justify-content-center mt-5">
+        <img class="col-md-3 border me-5" src="desk-customer-connect.webp" alt="" height="420" style="box-shadow: 10px 10px #ecefed;">            
+        <div class="col-md-4" style="margin-top: 3%">
             <p class="text-wrap fs-1 fw-bold">You need all the complaints in one place.</p>
             <p class="text-wrap fs-5 fw-medium">Resolving customer complaints requires a system with multichannel capabilities.We collect all support tickets from different channels and organizes them in one tab to help agents reply to all of them from one place</p>
         </div>
+    </div>
+</div>
+<div class="container mt-5">
+    <div class="row mx-5 d-flex justify-content-center mt-5">
+        <span class="fs-1 text-wrap fw-bold col-md-6 " style="width: auto,margin:auto">You need software that gives agents all the context they need</span>
+        <p class="text-wrap fs-5 fw-medium d-flex justify-content-center">Support agents need a streamlined view of all this data to maximize efficiency.</p>
+        <p class="text-wrap fs-5 fw-medium d-flex justify-content-center"> We will do that through our Agent Producitvity toolkit.</p>
     </div>
 </div>
 @endsection
