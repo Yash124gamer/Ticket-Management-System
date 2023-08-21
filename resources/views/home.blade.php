acf3d78452e65674679e3957881ecfe9ac2fa381
Yash124gamer <y325dde@gmail.com>
2023-08-21 16:24:52 +0530

    changed the home page for application

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,10 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
-            </div>
-        </div>
-    </div>
+<div class="container border border-secondary rounded " style="min-height: 60rem">
+    <x-statusBar />
+      <div class="container">
+          <x-main />
+      </div>
 </div>
 @endsection
