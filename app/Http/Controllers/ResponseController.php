a1ccc848b9287efee5b2c1d8d7215e79b14da430
Yash124gamer <y325dde@gmail.com>
2023-08-29 18:32:42 +0530

    updated the ResponseController to update the status of ticket to close when response is addeded

--- app/Http/Controllers/ResponseController.php
+++ app/Http/Controllers/ResponseController.php
@@ -12,12 +12,13 @@ class ResponseController extends Controller
         try {
             $response = $request->input('response');
             $ticketId = $request->input('ticketId');
-
+            $ticket = \App\Models\Tickets::find($ticketId);
             // Perform the insert operation using the Ticket model
             Response::create([
                 'response' => $response,
                 'ticket_id' => $ticketId
             ]);
+            $ticket->update(['status' => 'close']);
     
             return redirect()->back()->with('success', 'Response Added successfully');
         } catch (\Exception $e) {
